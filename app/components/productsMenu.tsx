b0d099f658d8e4610fd87f820f8d0eda91a1cd56
giorgi <gkiladze1994@gmail.com>
2024-05-09 22:40:14 +0300

    link color change

--- app/components/productsMenu.tsx
+++ app/components/productsMenu.tsx
@@ -10,8 +10,8 @@ export default function ProductsMenu () {
   return (
     <ul className="flex justify-start flex-col md:flex-row items-start mb-4 pt-12">
       {menuItems.map((item, idx) => (
-        <li key={idx} className={`min-w-52 md:min-w-40 md:mr-5 md:mb-0 mb-4 border border-lightGrey hover:bg-yellow hover:text-dark text-center ${pathname.includes(`/shop/category/${item.toLowerCase()}`) ? 'bg-yellow text-dark' : ''}`}>
-          <Link href={`/shop/category/${item.toLowerCase()}`} className="block w-full px-5 py-2 text-lg text-white font-medium tracking-widest">{item}</Link>
+        <li key={idx} className={`min-w-52 md:min-w-40 md:mr-5 md:mb-0 mb-4 border border-lightGrey hover:bg-yellow hover:text-dark text-center ${pathname.includes(`/shop/category/${item.toLowerCase()}`) ? 'bg-yellow' : ''}`}>
+          <Link href={`/shop/category/${item.toLowerCase()}`} className={`block w-full px-5 py-2 text-lg font-medium tracking-widest hover:text-dark ${pathname.includes(`/shop/category/${item.toLowerCase()}`) ? 'text-dark' : 'text-white'}`}>{item}</Link>
         </li>
       ))}
     </ul>
